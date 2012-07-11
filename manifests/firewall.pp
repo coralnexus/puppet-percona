9e77ce0ac14c1b40a94cd3aabcf3ac00b81646fc
Adrian Webb <adrian.webb@venturiscm.com>
2012-07-11 11:50:35 -0400

    Adding a new firewall rule to support the mysqlchk utility for the cluster server.

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -1,5 +1,12 @@
 
-class percona::firewall ( $port = $percona::params::port ) inherits percona::params {
+class percona::firewall (
+
+  $port          = $percona::params::port,
+  $mysqlchk_port = $percona::params::mysqlchk_port,
+
+) inherits percona::params {
+
+  #-----------------------------------------------------------------------------
 
   firewall { "400 INPUT Allow new MySQL connections":
     action => 'accept',
@@ -7,5 +14,15 @@ class percona::firewall ( $port = $percona::params::port ) inherits percona::par
     state  => 'NEW',
     proto  => 'tcp',
     dport  => $port,
+  }
+
+  if $mysqlchk_port {
+    firewall { "401 INPUT Allow new MySQL status check connections":
+      action => 'accept',
+      chain  => 'INPUT',
+      state  => 'NEW',
+      proto  => 'tcp',
+      dport  => $mysqlchk_port,
+    }
   }
 }
