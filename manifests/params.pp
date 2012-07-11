faf18d4de0c7d1baaee9d16dc50a46612af58c37
Adrian Webb <adrian.webb@venturiscm.com>
2012-07-11 11:49:31 -0400

    Adding new parameters to support Percona XtraDB CLuster and cleaning up some existing parameters.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,34 +1,50 @@
 
 class percona::params {
 
-  $client          = true
+  $percona_version    = '5.5'
 
-  $server          = false
-  $port            = 3306
-  $allow_remote    = true
+  $server_id          = 1
+  $server_ip          = $::ipaddress
+  $origin_ip          = ''
 
-  $percona_version = '5.5' # Options: 5.1, 5.5
+  $allow_remote       = true
 
-  $service   = 'mysql'
-  $user      = 'mysql'
-  $group     = 'mysql'
-  $logdir    = '/var/log/percona'
-  $socket    = '/var/lib/mysql/mysql.sock'
-  $datadir   = '/var/lib/mysql'
-  $targetdir = '/data/backups/mysql/'
-  $errorlog  = '/var/log/mysqld.log'
-  $pidfile   = '/var/run/mysqld/mysqld.pid'
+  $port               = 3306
+  $mysqlchk_port      = 9200
+
+  $cluster_check_user = 'clustercheckuser'
+  $cluster_check_pw   = 'clustercheckpassword!'
+
+  $service            = 'mysql'
+  $user               = 'mysql'
+  $group              = 'mysql'
 
   case $::operatingsystem {
     'debian', 'ubuntu': {
-      $confdir    = '/etc/mysql'
-      $config     = '/etc/mysql/my.cnf'
-      $pkg_client = "percona-xtradb-cluster-client-${percona_version}"
-      $pkg_server = "percona-xtradb-cluster-server-${percona_version}"
-      $pkg_common = [
+      $confdir           = '/etc/mysql'
+      $config            = '/etc/mysql/my.cnf'
+
+      $pkg_client        = "percona-xtradb-cluster-client-${percona_version}"
+      $pkg_client_ensure = 'present'
+
+      $pkg_server        = "percona-xtradb-cluster-server-${percona_version}"
+      $pkg_server_ensure = 'present'
+
+      $pkg_common        = [
+        'percona-xtrabackup',
         'percona-toolkit',
-        "percona-xtradb-cluster-common-${percona_version}",
       ]
+      $pkg_common_ensure = 'present'
+
+      $logdir            = '/var/log/percona'
+      $errorlog          = '/var/log/mysqld.log'
+
+      $socket            = '/var/lib/mysql/mysql.sock'
+      $datadir           = '/var/lib/mysql'
+
+      $pidfile           = '/var/run/mysqld/mysqld.pid'
+
+      $mysqlchk_daemon   = '/usr/bin/clustercheck'
     }
 
     default: {
