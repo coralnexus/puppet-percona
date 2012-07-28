24b98fb68a01814806821dc17e7c510013611ee9
Adrian Webb <adrian.webb@venturiscm.com>
2012-07-28 17:15:10 -0400

    Adding user management to the database definition so we can easily manage multiple databases on the same server dynamically.

--- manifests/database.pp
+++ manifests/database.pp
@@ -3,8 +3,14 @@ define percona::database (
 
   $database      = $name,
   $ensure        = 'present',
+  $host          = $percona::params::user_host,
   $port          = $percona::params::port,
   $sql_dump_file = $percona::params::database_sql_dump_file,
+  $user_name     = $name,
+  $password      = $name,
+  $permissions   = $percona::params::user_permissions,
+  $grant         = $percona::params::user_grant,
+  $remote        = $percona::params::allow_remote,
   $defaults_file = $percona::params::os_defaults_file,
 
 ) {
@@ -12,6 +18,7 @@ define percona::database (
   include percona
 
   #-----------------------------------------------------------------------------
+  # Database management
 
   Exec {
     path    => [ '/bin', '/usr/bin' ],
@@ -39,6 +46,29 @@ define percona::database (
       exec { "MySQL drop $database db":
         command => "mysql --defaults-file=${defaults_file} --port=${port} --execute=\"DROP DATABASE ${database}\";",
         onlyif  => "mysql --defaults-file=${defaults_file} --port=${port} --execute=\"SHOW DATABASES;\" | grep -x '${database}'",
+      }
+    }
+  }
+
+  #-----------------------------------------------------------------------------
+  # Database users
+
+  if $user_name {
+    Percona::User {
+      user_name => $user_name,
+      password  => $password,
+      database  => $database,
+      ensure    => present,
+      grant     => $grant,
+    }
+
+    percona::user { "${user_name}-local":
+      host => 'localhost',
+    }
+
+    if $remote == 'true' {
+      percona::user { "${user_name}-remote":
+        host => '%',
       }
     }
   }
