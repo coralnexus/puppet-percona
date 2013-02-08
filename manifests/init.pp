41f9f4da1987e114deaeabc5d5b4d2dbfc772395
Adrian Webb <adrian.webb@coraltech.net>
2013-02-08 02:28:00 -0500

    Adding new firewall capabilities, disabling selinux, and updating init parameters.

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,6 +44,7 @@ class percona (
   $config                           = $percona::params::config,
   $config_template                  = $percona::params::config_template,
   $conf_dir                         = $percona::params::conf_dir,
+  $configurations                   = $percona::params::configurations,
   $log_dir                          = $percona::params::log_dir,
   $error_log                        = $percona::params::error_log,
   $lib_dir                          = $percona::params::lib_dir,
@@ -55,26 +56,15 @@ class percona (
   $cluster_check_password           = $percona::params::cluster_check_password,
   $server_id                        = $percona::params::server_id,
   $server_ip                        = $percona::params::server_ip,
-  $origin_ip                        = $percona::params::origin_ip,
+  $origin_addresses                 = $percona::params::origin_addresses,
   $cluster_name                     = $percona::params::cluster_name,
   $allow_remote                     = $percona::params::allow_remote,
   $configure_firewall               = $percona::params::configure_firewall,
+  $percona_ports                    = $percona::params::percona_ports,
   $port                             = $percona::params::port,
   $mysqlchk_port                    = $percona::params::mysqlchk_port,
   $user                             = $percona::params::user,
   $group                            = $percona::params::group,
-  $skip_external_locking            = $percona::params::skip_external_locking,
-  $key_buffer_size                  = $percona::params::key_buffer_size,
-  $max_allowed_packet               = $percona::params::max_allowed_packet,
-  $table_open_cache                 = $percona::params::table_open_cache,
-  $sort_buffer_size                 = $percona::params::sort_buffer_size,
-  $read_buffer_size                 = $percona::params::read_buffer_size,
-  $read_rnd_buffer_size             = $percona::params::read_rnd_buffer_size,
-  $myisam_sort_buffer_size          = $percona::params::myisam_sort_buffer_size,
-  $thread_cache_size                = $percona::params::thread_cache_size,
-  $query_cache_size                 = $percona::params::query_cache_size,
-  $thread_concurrency               = $percona::params::thread_concurrency,
-  $skip_networking                  = $percona::params::skip_networking,
   $log_bin                          = $percona::params::log_bin,
   $binlog_format                    = $percona::params::binlog_format,
   $wsrep_provider                   = $percona::params::wsrep_provider,
@@ -89,26 +79,16 @@ class percona (
   $wsrep_auto_increment_control     = $percona::params::wsrep_auto_increment_control,
   $wsrep_causal_reads               = $percona::params::wsrep_causal_reads,
   $wsrep_notify_cmd                 = $percona::params::wsrep_notify_cmd,
-  $log_slave_updates                = $percona::params::log_slave_updates,
   $default_storage_engine           = $percona::params::default_storage_engine,
   $innodb_data_home_dir             = $percona::params::innodb_data_home_dir,
   $innodb_log_group_home_dir        = $percona::params::innodb_log_group_home_dir,
-  $innodb_buffer_pool_size          = $percona::params::innodb_buffer_pool_size,
-  $innodb_additional_mem_pool_size  = $percona::params::innodb_additional_mem_pool_size,
-  $innodb_log_buffer_size           = $percona::params::innodb_log_buffer_size,
   $innodb_flush_log_at_trx_commit   = $percona::params::innodb_flush_log_at_trx_commit,
-  $innodb_lock_wait_timeout         = $percona::params::innodb_lock_wait_timeout,
   $innodb_autoinc_lock_mode         = $percona::params::innodb_autoinc_lock_mode,
   $innodb_locks_unsafe_for_binlog   = $percona::params::innodb_locks_unsafe_for_binlog,
-  $mysql_no_auto_rehash             = $percona::params::mysql_no_auto_rehash,
-  $mysql_safe_updates               = $percona::params::mysql_safe_updates,
-  $mysqldump_quick                  = $percona::params::mysqldump_quick,
-  $mysqldump_max_allowed_packet     = $percona::params::mysqldump_max_allowed_packet,
-  $myisamchk_key_buffer_size        = $percona::params::myisamchk_key_buffer_size,
-  $myisamchk_sort_buffer_size       = $percona::params::myisamchk_sort_buffer_size,
-  $myisamchk_read_buffer            = $percona::params::myisamchk_read_buffer,
-  $myisamchk_write_buffer           = $percona::params::myisamchk_write_buffer,
-  $mysqlhotcopy_interactive_timeout = $percona::params::mysqlhotcopy_interactive_timeout,
+  $root_user                        = $percona::params::root_user,
+  $root_password                    = $percona::params::root_password,
+  $mysql_user                       = $percona::params::mysql_user,
+  $mysql_password                   = $percona::params::mysql_password
 
 ) inherits percona::params {
 
@@ -156,14 +136,8 @@ class percona (
       }
     }
 
-    if $mysqlchk_port {
-      firewall { "401 INPUT Allow new MySQL status check connections":
-        action => 'accept',
-        chain  => 'INPUT',
-        state  => 'NEW',
-        proto  => 'tcp',
-        dport  => $mysqlchk_port,
-      }
+    if ! empty($percona_ports) {
+      percona::rule { $percona_ports: }
     }
   }
 
@@ -195,28 +169,66 @@ class percona (
 
   #---
 
+  if ! $origin_addresses {
+    percona::query { 'update-root-password':
+      query         => "UPDATE user SET Password = PASSWORD('${root_password}') WHERE User = '${root_user}'; FLUSH PRIVILEGES",
+      access        => 'onlyif',
+      condition     => "SELECT * FROM user WHERE User = '${root_user}' AND Password != PASSWORD('${root_password}')",
+      port          => $port,
+      database      => 'mysql',
+    }
+
+    percona::user { $mysql_user:
+      password      => $mysql_password,
+      ensure        => 'present',
+      host          => 'localhost',
+      port          => $port,
+      database      => '*',
+      permissions   => 'ALL',
+      grant         => false,
+      defaults_file => undef,
+      root_user     => $root_user,
+      root_password => $root_password,
+      require       => Percona::Query['update-root-password'],
+    }
+  }
+
+  #---
+
   if $server and defined(Class['xinetd']) {
-    percona::user { $cluster_check_user:
-      password    => $cluster_check_password,
-      ensure      => 'present',
-      host        => 'localhost',
-      port        => $port,
-      database    => '*',
-      permissions => 'PROCESS',
-      grant       => false,
+    if ! $origin_ip {
+      percona::user { $cluster_check_user:
+        password    => $cluster_check_password,
+        ensure      => 'present',
+        host        => 'localhost',
+        port        => $port,
+        database    => '*',
+        permissions => 'PROCESS',
+        grant       => false,
+      }
     }
 
     xinetd::service { 'mysqlchk':
-      server         => $mysqlchk_daemon,
-      port           => $mysqlchk_port,
-      disable        => 'no',
-      flags          => 'REUSE',
-      socket_type    => 'stream',
-      wait           => 'no',
-      user           => 'nobody',
-      log_on_failure => 'USERID',
-      per_source     => 'UNLIMITED',
+      server             => $mysqlchk_daemon,
+      port               => $mysqlchk_port,
+      disable            => 'no',
+      flags              => 'REUSE',
+      socket_type        => 'stream',
+      wait               => 'no',
+      user               => 'nobody',
+      log_on_failure     => 'USERID',
+      per_source         => 'UNLIMITED',
+      configure_firewall => $configure_firewall,
+      service_ports      => [],
     }
+  }
+
+  #---
+
+  exec { "percona-disable-selinux":
+    path    => ["/usr/bin","/bin"],
+    command => "echo 0 >/selinux/enforce",
+    unless  => "grep 0 /selinux/enforce",
   }
 
   #-----------------------------------------------------------------------------
@@ -228,6 +240,25 @@ class percona (
       ensure  => $service_ensure,
       enable  => true,
       require => Package['percona_server'],
+    }
+  }
+}
+
+#-------------------------------------------------------------------------------
+
+define percona::rule( $port = $name ) {
+
+  $rule_description = "200 INPUT Allow Percona Cluster connections: $port"
+
+  #-----------------------------------------------------------------------------
+
+  if $port and ! defined(Firewall[$rule_description]) {
+    firewall { $rule_description:
+      action => accept,
+      chain  => 'INPUT',
+      state  => 'NEW',
+      proto  => 'tcp',
+      dport  => $port,
     }
   }
 }
