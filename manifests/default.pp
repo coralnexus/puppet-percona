eff622fd41a02805248d790cf6addd4ae3977e90
Adrian Webb (local admin) <adrian.webb@coralnexus.com>
2014-03-22 04:20:43 -0400

    Making some minor default configuration changes.

--- manifests/default.pp
+++ manifests/default.pp
@@ -56,7 +56,7 @@ class percona::default {
     "mysqld" => {
       "key_buffer_size" => "256M",
       "max_allowed_packet" => "1M",
-      "table_open_cache" => "256",
+      "table_open_cache" => "256M",
       "sort_buffer_size" => "1M",
       "read_buffer_size" => "1M",
       "read_rnd_buffer_size" => "4M",
@@ -64,7 +64,6 @@ class percona::default {
       "thread_cache_size" => "8",
       "thread_concurrency" => "8",
       "innodb_buffer_pool_size" => "256M",
-      "innodb_additional_mem_pool_size" => "20M",
       "innodb_log_buffer_size" => "8M",
       "innodb_lock_wait_timeout" => "50",
       "log-slave-updates" => "",
